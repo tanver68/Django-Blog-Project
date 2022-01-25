6a5c05af0b4b94966ab20268d155ed5e23251763
tanver68 <16101068@uap-bd.edu>
2022-01-25 13:21:49 +0600

    Database and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from curses.ascii import HT
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'Tanver',
-        'title' : 'Blog Post',
-        'content' : 'dhgfhg',
-        'date_posted' : 'January 23,2022' 
-    },
-     {
-        'author' : 'Rabby',
-        'title' : 'Blog Post',
-        'content' : 'gjh',
-        'date_posted' : 'January 24,2022' 
-    }
-]
+from .models import Post
 
 def home (request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
