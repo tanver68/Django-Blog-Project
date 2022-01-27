462f7997d0a8d8bc90ef305e89f61ea590674159
tanver68 <16101068@uap-bd.edu>
2022-01-27 15:18:49 +0600

    Create Login and Logout System

--- users/views.py
+++ users/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
+
+   # Registation system
+   
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account is created! Now you Login {username}!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+  # Profile System
+@login_required
+def profile(request):
+      return render(request, 'users/profile.html')
