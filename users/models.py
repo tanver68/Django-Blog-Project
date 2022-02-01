a4db4dc62efcf34b89379a0b37ed9dc7b665888f
tanver68 <16101068@uap-bd.edu>
2022-02-01 15:47:28 +0600

    Resize in image

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+#Resize in image
+from PIL import Image
 
 class profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,6 +11,18 @@ class profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} profile'
+    
+    #Resize in image
+    
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300 :
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
     
     
    
